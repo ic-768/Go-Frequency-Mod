9ef74793d48d093fe6c9af2b3435d92e36aa4bc4
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-09-23 21:36:43 +0300

    Formatting

--- freq_modulation.go
+++ freq_modulation.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-    "math"
-    "os"
-    "encoding/binary"
+	"encoding/binary"
+	"math"
+	"os"
 )
 
 func main() {
-    // Constants
-    sampleRate := 44100.0
-    duration := 3.0
-    frequency := 440.0
-    modFrequency := 5.0
-    modDepth := 3.0
+	// Constants
+	sampleRate := 44100.0
+	duration := 3.0
+	frequency := 440.0
+	modFrequency := 5.0
+	modDepth := 3.0
 
-    // Calculate number of samples
-    numSamples := int(sampleRate * duration)
+	// Calculate number of samples
+	numSamples := int(sampleRate * duration)
 
-    // Create a new WAV file
-    file, _ := os.Create("sine_wave.wav")
+	// Create a new WAV file
+	file, _ := os.Create("sine_wave.wav")
 
-    // Write WAV file header
-    writeWavHeader(file, numSamples, sampleRate)
+	// Write WAV file header
+	writeWavHeader(file, numSamples, sampleRate)
 
-    // Create modulator, generate and write audio samples
-    for i := 0; i < numSamples; i++ {
-        t := float64(i) / sampleRate
-        modulator := math.Sin(2.0 * math.Pi * t * modFrequency)
-        sample := math.Sin(2.0 * math.Pi * frequency * t + (modulator * modDepth ))
-        sampleInt := int16(sample * 32767)
-        binary.Write(file, binary.LittleEndian, sampleInt)
-    }
+	// Create modulator, generate and write audio samples
+	for i := 0; i < numSamples; i++ {
+		t := float64(i) / sampleRate
+		modulator := math.Sin(2.0 * math.Pi * t * modFrequency)
+		sample := math.Sin(2.0*math.Pi*frequency*t + (modulator * modDepth))
+		sampleInt := int16(sample * 32767)
+		binary.Write(file, binary.LittleEndian, sampleInt)
+	}
 
-    file.Close()
+	file.Close()
 }
 
 func writeWavHeader(file *os.File, numSamples int, sampleRate float64) {
-    // Constants
-    numChannels := 1
-    bitsPerSample := 16
+	// Constants
+	numChannels := 1
+	bitsPerSample := 16
 
-    // Calculate sub-chunk sizes
-    subChunk1Size := int32(16)            // Size of the PCM format subchunk
-    subChunk2Size := int32(numSamples * numChannels * bitsPerSample / 8) // Size of the data subchunk
+	// Calculate sub-chunk sizes
+	subChunk1Size := int32(16)                                           // Size of the PCM format subchunk
+	subChunk2Size := int32(numSamples * numChannels * bitsPerSample / 8) // Size of the data subchunk
 
-    fileSize := 36 + subChunk2Size
+	fileSize := 36 + subChunk2Size
 
-    // Write WAV file header
-    file.Write([]byte("RIFF"))
-    binary.Write(file, binary.LittleEndian, fileSize)
-    file.Write([]byte("WAVEfmt "))
-    binary.Write(file, binary.LittleEndian, subChunk1Size)
-    binary.Write(file, binary.LittleEndian, int16(1))                   // Audio format (PCM)
-    binary.Write(file, binary.LittleEndian, int16(numChannels))         // Number of channels
-    binary.Write(file, binary.LittleEndian, int32(int(sampleRate)))     // Sample rate
-    binary.Write(file, binary.LittleEndian, int32(int(sampleRate)*numChannels*bitsPerSample/8)) // Byte rate
-    binary.Write(file, binary.LittleEndian, int16(numChannels*bitsPerSample/8)) // Block align
-    binary.Write(file, binary.LittleEndian, int16(bitsPerSample))        // Bits per sample
-    file.Write([]byte("data"))
-    binary.Write(file, binary.LittleEndian, subChunk2Size)
+	// Write WAV file header
+	file.Write([]byte("RIFF"))
+	binary.Write(file, binary.LittleEndian, fileSize)
+	file.Write([]byte("WAVEfmt "))
+	binary.Write(file, binary.LittleEndian, subChunk1Size)
+	binary.Write(file, binary.LittleEndian, int16(1))                                           // Audio format (PCM)
+	binary.Write(file, binary.LittleEndian, int16(numChannels))                                 // Number of channels
+	binary.Write(file, binary.LittleEndian, int32(int(sampleRate)))                             // Sample rate
+	binary.Write(file, binary.LittleEndian, int32(int(sampleRate)*numChannels*bitsPerSample/8)) // Byte rate
+	binary.Write(file, binary.LittleEndian, int16(numChannels*bitsPerSample/8))                 // Block align
+	binary.Write(file, binary.LittleEndian, int16(bitsPerSample))                               // Bits per sample
+	file.Write([]byte("data"))
+	binary.Write(file, binary.LittleEndian, subChunk2Size)
 }
--- runAndBuild.sh
+++ runAndBuild.sh
@@ -1,3 +1,3 @@
-#!/bin/sh!
+#!/bin/sh
 
 go run freq_modulation.go && vlc sine_wave.wav
